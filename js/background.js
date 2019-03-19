c200473f7aebe656e2dc4e24d4192f0bb53761a9
xialeiCP9 <2278769715@qq.com>
2019-03-19 22:13:46 +0800

    update + create

---
+++ js/background.js
@@ -0,0 +1,31 @@
+(function(){
+	var Background = window.Background = function(){
+		this.image = game.R["bg_day"];
+		this.y = game.canvas.height * 368 / 512 - 368;
+		//移动
+		this.x = 0;
+		//移动速度
+		this.speed = 1;
+		this.w = 288;
+		this.h = 512;
+	}
+	Background.prototype.update = function(){
+		this.x -= this.speed;
+		if(this.x < -this.w){
+			this.x = 0;
+		}
+	}
+	Background.prototype.render = function() {
+		//渲染图片
+		game.ctx.drawImage(this.image,this.x,this.y);
+		game.ctx.drawImage(this.image,this.x + this.w,this.y);
+		game.ctx.drawImage(this.image,this.x + this.w * 2,this.y);
+		//渲染上/下的颜色
+		//天空颜色
+		game.ctx.fillStyle="#4EC0CA";
+		game.ctx.fillRect(0,0,game.canvas.width,this.y+50);
+		//大地颜色
+		game.ctx.fillStyle="#5EE270";
+		game.ctx.fillRect(0,this.y+this.h - 50,game.canvas.height,game.canvas.height-this.y-this.h+50);
+	};
+})();
--- js/game.js
+++ js/game.js
@@ -2,14 +2,47 @@
 	var Game = window.Game = function(id){
 		this.canvas = document.getElementById("canvas");
 		this.ctx = canvas.getContext("2d");
-		this.resources = {};
+		this.R = {};
+		this.FNO = 0;//帧编号
+		this.init();
 	};
 
 	Game.prototype.init = function(){
+		var self = this;
+		var width = document.documentElement.clientWidth;
+		var height = document.documentElement.clientHeight;
+		if(width > 414){
+			width = 414;
+		} else if(width < 320){
+			width = 320
+		}
+		if(height > 812){
+			height = 812;
+		} else if(height < 500){
+			height = 500
+		}
+		this.canvas.width = width;
+		this.canvas.height = height;
 		this.loadAllResources(function(){
-			console.log("Oh Yeah")
+			self.start();
 		});
 	}
+	Game.prototype.start = function() {
+		var self = this;
+		this.background = new Background();
+		setInterval(function(){
+			//清空canvas
+			self.ctx.clearRect(0,0,self.canvas.width,self.canvas.height);
+			//更新背景图片位置
+			self.background.update();
+			//渲染图片
+			self.background.render();
+			self.FNO++;
+			self.ctx.font = "16px Arial";
+			self.ctx.textAlign = "left";
+			self.ctx.fillText("FNO:"+self.FNO,10,20);
+		},20);
+	};
 	/**
 	 * 首先读取所有资源文件
 	 * @param  {Function} callback [异步操作，读取完毕后，才能开启定时器]
@@ -28,15 +61,18 @@
 		xhr.onreadystatechange = function(){
 			if(xhr.readyState == 4 && xhr.status == 200){
 				var result = JSON.parse(xhr.responseText);
-				var len = getJSONLen(result);
-				for(var k in result){
-					self.resources[k] = new Image();
-					self.resources[k].src = result[k];
-					self.ctx.fillText("正在加载第" + count +"张图片...",self.canvas.width / 2,self.canvas.height * (1-0.618));
-					self.resources[k].onload = function(){
+				var len = result.images.length;
+				for(var i=0;i<len;i++){
+					self.R[result.images[i].name] = new Image();
+					self.R[result.images[i].name].src = result.images[i].url;
+					
+					self.R[result.images[i].name].onload = function(){
 						count++;
 						self.ctx.clearRect(0,0,self.canvas.width,self.canvas.height);
-						self.ctx.fillText("正在加载第" + count +"张图片...",self.canvas.width / 2,self.canvas.height * (1-0.618));
+						self.ctx.font = "20px 微软雅黑";
+						self.ctx.textAlign = "center";
+						var text = "正在加载资源 " + count + "/" + len + " ,请稍后..."
+						self.ctx.fillText(text,self.canvas.width / 2,self.canvas.height * (1-0.618));
 						if(count == len){
 							callback && callback();
 							console.log("加载完毕");
@@ -46,14 +82,6 @@
 			}
 		}
 		xhr.open("GET","./R.json",true);
-		xhr.send();
-	}
-
-	function getJSONLen(json){
-		var len = 0;
-		for(var k in json){
-			len ++;
-		}
-		return len;
+		xhr.send(null);
 	}
 })();
